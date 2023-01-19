52b027c4547c035f982d2a37c9f42ad810af99f6
karpukhinp01 <88552184+karpukhinp01@users.noreply.github.com>
2023-01-19 17:52:49 +0100

    AndroidManifest.xml internet Connection FirstFragment.kt appBar row_item_design.xml fixed height

--- app/src/main/java/com/example/nousassesment/FirstFragment.kt
+++ app/src/main/java/com/example/nousassesment/FirstFragment.kt
@@ -5,14 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.nousassesment.databinding.FragmentFirstBinding
+import com.example.nousassesment.viewmodels.MainViewModel
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
 
+    private lateinit var  RVAdapter: RecyclerViewAdapter
+    private lateinit var mMainViewModel: MainViewModel
     private var _binding: FragmentFirstBinding? = null
 
     // This property is only valid between onCreateView and
@@ -32,6 +37,15 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val recyclerview = binding.recyclerView
+        recyclerview.layoutManager = GridLayoutManager(requireContext(), 2)
+        RVAdapter = RecyclerViewAdapter(requireContext())
+        recyclerview.adapter = RVAdapter
+
+        mMainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        mMainViewModel.programmeList.observe(viewLifecycleOwner) {
+            RVAdapter.updatePosts(it)
+        }
     }
 
     override fun onDestroyView() {
