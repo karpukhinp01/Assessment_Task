e5ebfc8c37a4ee291b3a4fc1ab77090f24dd63e3
karpukhinp01 <88552184+karpukhinp01@users.noreply.github.com>
2023-01-19 17:35:00 +0100

    RecyclerViewAdapter.kt

---
+++ app/src/main/java/com/example/nousassesment/RecyclerViewAdapter.kt
@@ -0,0 +1,62 @@
+package com.example.nousassesment
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.example.nousassesment.data.Item
+import com.squareup.picasso.Picasso
+
+class RecyclerViewAdapter(val context: Context): RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+
+    var mList = ArrayList<Item>()
+
+    // Holds the views for adding it to image and text
+    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+        val image: ImageView = itemView.findViewById(R.id.image)
+        val title: TextView = itemView.findViewById(R.id.title)
+//        val description: TextView = itemView.findViewById(R.id.desc)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        // inflates the card_view_design view
+        // that is used to hold list item
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.row_item_design, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    // binds the list items to a view
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val currentItem = mList[position]
+
+
+        val imageUrl = if (currentItem.imageUrl.endsWith("/preview")) currentItem.imageUrl
+        else currentItem.imageUrl.plus("/preview")
+        // sets the image to the imageview from our itemHolder class
+        holder.title.text = currentItem.title
+        Picasso.with(context).load(imageUrl).into(holder.image)
+
+//        // sets the text to the textview from our itemHolder class
+//        holder.description.text = currentItem.description
+    }
+
+    fun updatePosts(postList: List<Item>) {
+        this.mList.clear()
+        this.mList.addAll(postList)
+        notifyDataSetChanged()
+    }
+
+    // return the number of the items in the list
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+}
