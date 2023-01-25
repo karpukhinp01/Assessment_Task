443e3e475fcc74907d138f3307bc207a7e587d14
karpukhinp01 <88552184+karpukhinp01@users.noreply.github.com>
2023-01-25 14:42:26 +0100

    liveData tests, mainViewmodel comments

--- app/src/main/java/com/example/nousassesment/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/nousassesment/viewmodels/MainViewModel.kt
@@ -32,6 +32,8 @@ class MainViewModel(private val repository: MainApi): ViewModel() {
         getItemsList()
     }
 
+    // This function is responsible for fetching a list of items from the repository and updating the
+    // programmeList LiveData with the received items.
     fun getItemsList() {
         viewModelScope.launch {
             _loadStatus.value = LoadStatus.LOADING
@@ -46,6 +48,7 @@ class MainViewModel(private val repository: MainApi): ViewModel() {
         }
     }
 
+    // This function filters the items based on a given query string.
     fun filter(query: String): ArrayList<Item> {
         val filteredList = ArrayList<Item>()
         if (programmeList.value != null) {
@@ -57,6 +60,8 @@ class MainViewModel(private val repository: MainApi): ViewModel() {
         }
         return filteredList
     }
+
+    // This function retrieves a bitmap image from a given URL.
     private fun getBitMap(urlS: String): Bitmap? {
         return try {
             val url = URL(urlS)
@@ -66,14 +71,23 @@ class MainViewModel(private val repository: MainApi): ViewModel() {
         }
     }
 
+    // This function saves a given bitmap image to a specific location in the device's external cache directory.
     private fun saveBitMap(image: Bitmap, context: Context): Uri {
+
+        // We first create a new directory called "my_images" in the external cache directory.
+        // This is to ensure that the images are stored in a dedicated folder of the external cache.
         val cachePath = File(context.externalCacheDir, "my_images/")
         cachePath.mkdirs()
 
+        // Next, we create a new file with the name "shared_media.png" in the newly created directory.
+        // This file will be used to save the image.
         val file = File(cachePath, "shared_media.png")
         val fileOutputStream: FileOutputStream
 
         try {
+
+            // We open a file output stream for the file and compress
+            // the image with PNG format and 100% quality.
             fileOutputStream = FileOutputStream(file)
             image.compress(Bitmap.CompressFormat.PNG, 100, fileOutputStream)
             fileOutputStream.flush()
@@ -81,19 +95,34 @@ class MainViewModel(private val repository: MainApi): ViewModel() {
         } catch (e: Exception) {
             Log.d("saveBitMap", e.message.toString())
         }
+
+        // Finally, we return the Uri of the saved file using the FileProvider.getUriForFile method.
         return FileProvider.getUriForFile(context, context.packageName + ".provider", file)
     }
 
+    // This function is responsible for creating an intent to send an email with an attachment.
+    // It takes in two parameters, the item that needs to be shared and the context of the application.
     fun sendEmail(item:Item, context: Context): Intent {
 
+        // First, it retrieves the image associated with the
+        // item by calling the getBitMap function passing the item's imageUrl.
         val image = getBitMap(item.imageUrl)
         var uri: Uri? = null
+        // Next, it saves the image to the external
+        // cache directory by calling the saveBitMap function, passing the image and the context, if
+        // the image is not null.
         if (image != null) {
             uri = saveBitMap(image, context)
         }
 
+        // Then, it creates an Intent with the action Intent.ACTION_SEND
+        // and sets the type to "message/rfc822" to indicate that the intent is for sending an email.
         val intent = Intent(Intent.ACTION_SEND)
         intent.type = "message/rfc822"
+
+
+        // The intent's extras are set with the item's title as the subject,
+        // item's description as the text body, and the Uri of the saved image as the attachment.
         intent.putExtra(Intent.EXTRA_STREAM, uri)
         intent.putExtra(Intent.EXTRA_SUBJECT, item.title)
         intent.putExtra(Intent.EXTRA_TEXT, item.description)
--- app/src/test/java/com/example/nousassesment/viewmodels/MainViewModelTest.kt
+++ app/src/test/java/com/example/nousassesment/viewmodels/MainViewModelTest.kt
@@ -1,16 +1,17 @@
 package com.example.nousassesment.viewmodels
 
+import android.os.Looper
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import org.junit.Assert.*
 import com.example.nousassesment.data.Item
 import com.example.nousassesment.data.ItemList
+import com.example.nousassesment.data.LoadStatus
 import com.example.nousassesment.repositories.MainApi
-import io.mockk.coEvery
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.spyk
+import io.mockk.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.setMain
+import org.junit.Rule
 
 import org.junit.Test
 
@@ -19,19 +20,22 @@ class MainViewModelTest {
 
     val dispatcher = TestCoroutineDispatcher()
 
+    val item1 = Item("id1", "title1", "description1", "https://example.com/test_item1.png")
+    val item2 = Item("id2", "title2", "description2", "https://example.com/test_item2.png")
+    val item3 = Item("id3", "title3", "description3", "https://example.com/test_item3.png")
+    val itemList = listOf(item1, item2, item3)
+
+
     @Test
     fun `filter should return filtered items based on query`() {
         Dispatchers.setMain(dispatcher)
         val query = "1"
 
-        val item1 = Item("id1", "title1", "description1", "https://example.com/test_item1.png")
-        val item2 = Item("id2", "title2", "description2", "https://example.com/test_item2.png")
-        val item3 = Item("id3", "title3", "description3", "https://example.com/test_item3.png")
 
         val repository = mockk<MainApi>()
-        coEvery { repository.getItems() } returns ItemList(listOf(item1, item2, item3))
+        coEvery { repository.getItems() } returns ItemList(itemList)
 
-        val viewModel = spyk(MainViewModel(repository) )
+        val viewModel = spyk(MainViewModel(repository))
         every { viewModel.programmeList.value } returns listOf(item1, item2, item3)
 
         // Act
@@ -41,5 +45,31 @@ class MainViewModelTest {
         assertEquals(1, filteredList.size)
         assertEquals("title1", filteredList[0].title)
         assertEquals("description1", filteredList[0].description)
+    }
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @Test
+    fun `getItemsList should update programmeList LiveData`() {
+        Dispatchers.setMain(dispatcher)
+
+        mockkStatic(Looper::class)
+        every { Looper.getMainLooper() } returns mockk()
+
+        val repository = mockk<MainApi>()
+        coEvery { repository.getItems() } returns ItemList(listOf(item1, item2, item3))
+
+        val viewModel = MainViewModel(repository)
+
+        //Act
+        viewModel.getItemsList()
+        viewModel.programmeList.observeForever {  }
+        viewModel.loadStatus.observeForever {  }
+
+        // Assert
+        assertEquals(viewModel.programmeList.value, itemList)
+        assertEquals(viewModel.loadStatus.value, LoadStatus.SUCCESS)
+
     }
 }
